b9c50f0d48e9ca7a0aa4e097dd6c685bb9b445a4
Jack Yen <jyen@ti.com>
2012-04-05 13:02:54 -0500

    bltsville: update bvbuffdesc and bvsurfgeom
    This patch depends on:
    http://review.omapzoom.org/#/c/21399/ -- external/kernel-header
    
    Change-Id: I898e8824c34b641bd399b90bc375dddd45d104c4
    Signed-off-by: Jack Yen <jyen@ti.com>

--- libc/kernel/common/linux/bvbuffdesc.h
+++ libc/kernel/common/linux/bvbuffdesc.h
@@ -19,17 +19,39 @@
 #ifndef BVBUFFDESC_H
 #define BVBUFFDESC_H
 struct bvbuffmap;
-struct bvbuffdesc {
+#define BVATDEF_VENDOR_SHIFT 24
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define BVATDEF_VENDOR_MASK (0xFF << BVATDEF_VENDOR_SHIFT)
+#define BVATDEF_VENDOR_ALL (0x00 << BVATDEF_VENDOR_SHIFT)
+#define BVATDEF_VENDOR_TI (0x01 << BVATDEF_VENDOR_SHIFT)
+enum bvauxtype {
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ BVAT_NONE = 0,
+ BVAT_PHYSDESC =
+ BVATDEF_VENDOR_ALL + 1,
+#ifdef BVAT_EXTERNAL_INCLUDE
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#include BVAT_EXTERNAL_INCLUDE
+#endif
+};
+struct bvphysdesc {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
  unsigned int structsize;
- void *virtaddr;
- unsigned long length;
- struct bvbuffmap *map;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
  unsigned long pagesize;
  unsigned long *pagearray;
  unsigned int pagecount;
- unsigned long pageoffset;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ unsigned long pageoffset;
+};
+struct bvbuffdesc {
+ unsigned int structsize;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ void *virtaddr;
+ unsigned long length;
+ struct bvbuffmap *map;
+ enum bvauxtype auxtype;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ void *auxptr;
 };
 #endif
+
